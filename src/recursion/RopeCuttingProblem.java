83754c9609579ef162c10e4d0bb42a82cd3707c2
Anuj <akg.cs.engg@gmail.com>
2023-03-13 19:48:49 +0530

    Coded Rope Cutting Problem

--- src/recursion/RopeCuttingProblem.java
+++ src/recursion/RopeCuttingProblem.java
@@ -1,5 +1,7 @@
 package recursion;
 
+import java.util.Arrays;
+
 /**
  * Given a rope of length n, you need to find the maximum number of pieces
  * you can make such that the length of every piece is in set {a, b, c} for
@@ -8,17 +10,46 @@ package recursion;
  * https://stackoverflow.com/questions/63250075/find-the-maximum-number-of-pieces-a-rod-can-be-cut
  */
 public class RopeCuttingProblem {
-    static int maxCuts(int n, int a, int b, int c){
+
+    //To optimize the recursive call, we can create 1D array
+    static Integer[] memo;
+
+    /**
+     *
+     */
+    static int maxCutsUsingRecursion(int n, int a, int b, int c){
         //Base Conditions
         if(n < 0) return -1;
         if(n == 0) return 0;
 
-        int res =  max(maxCuts(n-a,a,b,c), maxCuts(n-b,a,b,c), maxCuts(n-c,a,b,c));
+        int res =  max(maxCutsUsingRecursion(n-a,a,b,c), maxCutsUsingRecursion(n-b,a,b,c), maxCutsUsingRecursion(n-c,a,b,c));
 
         //Boundary Conditions
         if(res == -1) return -1;
 
         return (1 + res);
+    }
+
+    /**
+     *      
+     */
+    static int maxCutsUsingDP(int n, int a, int b, int c){
+
+        if(memo == null) {
+            memo = new Integer[n+1];
+        }
+        //Base Conditions
+        if(n < 0) return -1;
+        if(n == 0) return 0;
+
+        if(memo[n] != null) return memo[n];
+
+        int res =  max(maxCutsUsingDP(n-a,a,b,c), maxCutsUsingDP(n-b,a,b,c), maxCutsUsingDP(n-c,a,b,c));
+
+        //Boundary Conditions
+        memo[n] = (res == -1) ?  -1 : (res+1);
+
+        return memo[n];
     }
 
     static int max(int... num){
@@ -29,8 +60,21 @@ public class RopeCuttingProblem {
         return max;
     }
     public static void main(String[] args) {
-        System.out.println(maxCuts(5, 2,5,1));
-        System.out.println(maxCuts(23, 12,9,11));
-        System.out.println(maxCuts(5, 4,2,6));
+        //Using Recursion
+        System.out.println(maxCutsUsingRecursion(5, 2,5,1));
+        System.out.println(maxCutsUsingRecursion(23, 12,9,11));
+        System.out.println(maxCutsUsingRecursion(5, 4,2,6));
+        System.out.println(maxCutsUsingRecursion(9, 2,2,2));
+
+        //Using Dynamic Programming
+        System.out.println();
+        memo =null;
+        System.out.println(maxCutsUsingDP(5, 2,5,1));
+        memo =null;
+        System.out.println(maxCutsUsingDP(23, 12,9,11));
+        memo =null;
+        System.out.println(maxCutsUsingDP(5, 4,2,6));
+        memo =null;
+        System.out.println(maxCutsUsingDP(9, 2,2,2));
     }
 }
